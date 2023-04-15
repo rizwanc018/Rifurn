a0c5f55a5186592e6bd7e4076da27ba80e16f5c5
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-04-15 08:36:28 +0530

    On branch master 	modified:   helpers/adminHelper.js
    reject() added on promises

--- helpers/adminHelper.js
+++ helpers/adminHelper.js
@@ -26,13 +26,13 @@ const adminHelper = {
     deleteUser: (id) => {
         return new Promise(async (resolve, reject) => {
             const data = await Usermodel.deleteOne({ _id: id })
-            resolve(data)
+            data ? resolve(data) : reject("Something wrong")
         })
     },
     getUser: (id) => {
         return new Promise(async (resolve, reject) => {
             const data = await Usermodel.findById(id)
-            resolve(data)
+            data ? resolve(data) : reject("Not found")
         })
     },
     updateUser: (id, data) => {
@@ -70,53 +70,6 @@ const adminHelper = {
             }
         })
     },
-    // addProduct: (req, res) => {
-    //     const imageId = []
-    //     const { productName, productCategory, productPrice, stock, productDescription } = req.body
-    //     return new Promise(async (resolve, reject) => {
-    //         try {
-    //             for (const file of req.files) {
-    //                 const result = await cloudinary.uploader.upload(file.path);
-    //                 fs.unlinkSync(file.path)
-    //                 imageId.push(result.public_id)
-    //             }
-    //             productModel.create({ productName: productName, category: productCategory, price: productPrice, stock: stock, description: productDescription, images: imageId })
-    //                 .then(() => {
-    //                     resolve("Product added successfully")
-    //                 }).catch(() => {
-    //                     reject("Unable to add product")
-    //                 })
-    //         } catch (error) {
-    //             reject(error)
-    //         }
-    //     })
-    // },
-    // deleteProduct: (req) => {
-    //     const imageIds = req.query.img.split(",")
-    //     return new Promise(async (resolve, reject) => {
-    //         const result = await productModel.deleteOne({ _id: req.params.id })
-    //         if (result.acknowledged) {
-    //             for (const imageId of imageIds) {
-    //                 cloudinary.uploader.destroy(imageId, (err, res) =>{
-    //                     console.log(err, res);
-    //                 })
-    //                     .then(resp => resolve(resp))
-    //                     .catch(_err => reject("Something went wrong, please try again later."));
-    //             }
-    //         } else {
-    //             reject("Something went wrong, please try again later.")
-    //         }
-
-    //     })
-    // },
-    // getAllProducts: () => {
-    //     return new Promise((resolve, reject) => {
-    //         productModel.find({})
-    //             .then(products => {
-    //                 resolve(products)
-    //             })
-    //     })
-    // }
 }
 
 export default adminHelper
